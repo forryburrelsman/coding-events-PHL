79a307f5ead91a5a8ce8c31a1f213b8effa2e0cc
Forrest Burrelsman <forrestburrelsman@gmail.com>
2021-10-25 13:23:52 -0500

    create and render form, add a form handler method

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,12 @@
 package org.launchcode.codingevents.controllers;
 
+import com.fasterxml.jackson.databind.deser.std.CollectionDeserializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +15,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> eventsList = new ArrayList<>();
+
     @GetMapping
     public String displayEvents(Model model){
-        List<String> eventsList = new ArrayList<>();
-        eventsList.add("Conference");
-        eventsList.add("Birthday Party");
-        eventsList.add("Fundraiser");
         model.addAttribute("events", eventsList);
         return "events/index";
+    }
+
+    //form
+    @GetMapping("create") //lives at events/create
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    //form handler method
+    @PostMapping("create") //lives at /events/create
+    public String createEvent(@RequestParam String eventName, Model model) {
+        eventsList.add(eventName);
+        model.addAttribute("eventName", eventName);
+        return "redirect:";
     }
 }
